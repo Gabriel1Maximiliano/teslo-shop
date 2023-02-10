371eeee801c6f810f96d40c86b1005af7668b5a1
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-02-10 17:53:40 -0300

    folder structure added and ShowLayout component created

---
+++ components/layouts/ShopLayouts.tsx
@@ -0,0 +1,42 @@
+import Head from "next/head"
+
+
+
+interface Props {
+    title:string;
+    pageDescription:string;
+    imageFullUrl?: string;
+    children?: any;
+}
+export const ShopLayouts = ({ children,title,pageDescription,imageFullUrl  }:Props) => {
+  return (
+    <>
+    <Head>
+        <title>{ title }</title>
+        <meta name="decription" content={ pageDescription } />
+        <meta name="og:title" content={ pageDescription } />
+        <meta name="og:description" content={ pageDescription } />
+        {
+            imageFullUrl && (
+                <meta name="og:iamge" content={ imageFullUrl } />
+            )
+        }
+
+    </Head>
+
+    <nav>
+        {/* NavBar */}
+    </nav>
+
+    {/* todo : SideBar */}
+
+    <main style={{ margin:'80px auto', maxWidth:'1440px', padding:'0px 30px' }} >
+        { children }
+    </main>
+    {/* footer */}
+    <footer>
+        {/* mi custom footer */}
+    </footer>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 
+import { Typography } from '@mui/material'
 import { Inter } from '@next/font/google'
+import { ShopLayouts } from 'components/layouts'
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-  <h1>home page</h1>
-    </>
+   <ShopLayouts title={'Teslo-Shop-Home'} pageDescription={'Encuentra los mejores productos de Teslo-Shop'}>
+      <Typography variant='h1' component='h1' >Tienda</Typography>
+      <Typography variant='h2' sx={ {marginBottom:1 } } >Todos los productos</Typography>
+   </ShopLayouts>
   )
 }
