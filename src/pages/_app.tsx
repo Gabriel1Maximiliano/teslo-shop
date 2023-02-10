50b493797f6db383b9d63e548be7c7fd0a33bf80
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-02-10 17:34:33 -0300

    Material UI installed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { lightTheme } from '../../themes/light-theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ lightTheme } >
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
