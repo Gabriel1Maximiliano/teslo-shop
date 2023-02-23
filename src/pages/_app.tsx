83b4c30ed4a2bf644646e1bca46aac3d89bc8461
Gabriel1Maximiliano <gabrielhmloza@gmail.com>
2023-02-23 00:58:51 -0300

    se agrega paypal y su boton de pago

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,17 +7,21 @@ import { UIProvider } from 'context/ui';
 import type { AppProps } from 'next/app'
 import { SWRConfig } from 'swr';
 import { lightTheme } from '../../themes/light-theme';
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  console.log( 'variable desde App'+process.env.NEXT_AUHT_PAYPAL_CLIENT_ID)
   return (
 <SessionProvider>
+  <PayPalScriptProvider options={{ "client-id": 'ARST-UyBmH4ArupBqPasgRjfxkpNo2VFg8RG4X2z3ugsGJqE5q7ONYBo6Fy-jsQOt5WD6XUj1a3LeuQE' || '' }} >
     <SWRConfig 
     value={{
       
       fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
     }}
   >
-    <AuthProvider>
+      <AuthProvider>
         <CartProvider>
             <UIProvider>
               <ThemeProvider theme={ lightTheme } >
@@ -28,6 +32,7 @@ export default function App({ Component, pageProps }: AppProps) {
         </CartProvider>
       </AuthProvider>
     </SWRConfig>
+  </PayPalScriptProvider>
     </SessionProvider>    
   )
 }
